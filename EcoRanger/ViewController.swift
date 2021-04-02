172bfcdb11a6b7649161684c8bec21cad68f9aae
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-04-03 06:11:25 +0700

    Initial Commit

---
+++ EcoRanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EcoRanger
+//
+//  Created by Syahrul Apple Developer BINUS on 03/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
