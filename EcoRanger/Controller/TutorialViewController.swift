8230c34c9b02f70e471ed740d003f88090f00c66
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-04-03 19:56:58 +0700

    Add basic layouting and dummy button action to tutorial storyboard

---
+++ EcoRanger/Controller/TutorialViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TutorialViewController.swift
+//  EcoRanger
+//
+//  Created by Syahrul Apple Developer BINUS on 03/04/21.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func prevButtonPressed(_ sender: UIButton) {
+        print("Prev Button Pressed")
+    }
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        print("Next Button Pressed")
+    }
+    
+}
