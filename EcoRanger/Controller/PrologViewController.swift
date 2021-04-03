91cf1432ed81cde243408ef9c2ea7793bf7ac261
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-04-03 19:23:33 +0700

    Add dummy button to prologviewcontroller

--- EcoRanger/Controller/PrologViewController.swift
+++ EcoRanger/Controller/PrologViewController.swift
@@ -15,15 +15,11 @@ class PrologViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func prevButtonPressed(_ sender: UIButton) {
+        print("Prev Button Pressed!")
     }
-    */
-
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        print("Next Button Pressed!")
+    }
+    
 }
