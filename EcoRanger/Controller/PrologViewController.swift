bd4206042cb9a9fed94067f65e2f6583fdfa7175
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-04-03 18:47:14 +0700

    Add new assets and basic layouting for prolog storyboard

---
+++ EcoRanger/Controller/PrologViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PrologViewController.swift
+//  EcoRanger
+//
+//  Created by Syahrul Apple Developer BINUS on 03/04/21.
+//
+
+import UIKit
+
+class PrologViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
