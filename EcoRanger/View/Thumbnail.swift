78f2932607d229c3d5a422e784a169d70b26c969
EcoRanger <adiputrachristian17@gmail.com>
2021-04-05 00:28:12 +0700

    - Thumbnail collection view

---
+++ EcoRanger/View/Thumbnail.swift
@@ -0,0 +1,14 @@
+//
+//  Thumbnail.swift
+//  EcoRanger
+//
+//  Created by Christian Adiputra on 05/04/21.
+//
+
+import Foundation
+struct Thumbnail {
+    var id: String?
+    var chapter: String?
+    var story: String?
+    var imgthumbnail: String?
+}
