a5092e8652d87d092aa4ad2894794dd682313efc
samberutu <45347400+samberutu@users.noreply.github.com>
2021-04-05 09:54:08 +0700

    popUpView setting button rul

--- EcoRanger/Controller/MainViewController.swift
+++ EcoRanger/Controller/MainViewController.swift
@@ -8,20 +8,75 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    
+    //popUp var space
+    @IBOutlet var settingView: UIView!
+    @IBOutlet var blurView: UIVisualEffectView!
+    @IBAction func backButton(_ sender: UIButton) {
+            popUpTransitionOut(desiredView: settingView)
+            
+        }
+    @IBAction func sfxButton(_ sender: Any) {
+        print("ini switch sfx")
+        
+    }
+    @IBAction func bgmSwitch(_ sender: Any) {
+        print("ini switch BMG")
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //popUp setting button
+        blurView.bounds = self.view.bounds
+        settingView.layer.cornerRadius = 10
+        
     }
 
     @IBAction func settingButtonPressed(_ sender: UIButton) {
-        print("Setting Button Pressed")
+        //print("Setting Button Pressed")
+        //action
+        //popUpTransition(desiredView: blurView)
+        popUpTransitionIn(desiredView: settingView)
+        
+        
     }
     @IBAction func badgesButtonPressed(_ sender: UIButton) {
         print("Badges Button Pressed")
     }
     @IBAction func avatarButtonPressed(_ sender: UIButton) {
         print("Avatar Button Pressed")
+    }
+    
+    //popUp transition
+    func popUpTransitionIn(desiredView:UIView){
+        let bgView  = self.view!
+        
+        //
+        bgView.addSubview(desiredView)
+        
+        //
+        desiredView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        desiredView.alpha = 0
+        desiredView.center = bgView.center
+        
+        //
+        UIView.animate(withDuration: 0.3, animations: {
+            desiredView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            desiredView.alpha = 1
+        })
+    }
+    
+    func popUpTransitionOut(desiredView:UIView){
+        //
+        UIView.animate(withDuration: 0.3, animations: {
+            desiredView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            desiredView.alpha = 1
+        }, completion: { _ in
+            desiredView.removeFromSuperview()
+        })
     }
     
 }
