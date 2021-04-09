0dfd45387fca35e100da9ac0be2d740472c2208f
samberutu <45347400+samberutu@users.noreply.github.com>
2021-04-09 12:08:54 +0700

    popup untuk result game (failure/success) udah jalan

---
+++ EcoRanger/Controller/GameResultPopUpController.swift
@@ -0,0 +1,90 @@
+//
+//  GameResultPopUpController.swift
+//  EcoRanger
+//
+//  Created by Samlo Berutu on 09/04/21.
+//
+
+import UIKit
+
+class GameResultPopUpController: UIView {
+
+    static let instance = GameResultPopUpController()
+    @IBOutlet var parentView:UIView!
+    @IBOutlet var childView:UIView!
+    @IBOutlet var starView:UIImageView!
+    @IBOutlet var trueBtn:UIButton!
+    @IBOutlet var falseBtn:UIButton!
+    @IBOutlet var nextBtn:UIButton!
+    @IBOutlet var succesLbl:UILabel!
+    @IBOutlet var failureLbl:UILabel!
+    
+    @IBAction func trueButton(_ sender: Any) {
+        print("resume button")
+        
+    }
+    @IBAction func falseButton(_ sender: Any) {
+        print("home button")
+        parentView.removeFromSuperview()
+        
+    }
+    @IBAction func nextButton(_ sender: Any) {
+        print("setting button")
+        parentView.removeFromSuperview()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        Bundle.main.loadNibNamed("GameResultPopUp", owner: self, options: nil)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    enum AlertType{
+        case success
+        case failure
+    }
+    
+    private func commonInit(){
+        trueBtn.layer.cornerRadius = 5
+        falseBtn.layer.cornerRadius = 5
+        nextBtn.layer.cornerRadius = 5
+        childView.layer.cornerRadius = 10
+        
+        parentView.bounds = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        
+        parentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+    
+    func showResultView(alertType: AlertType){
+        print("Result View")
+        if alertType == .success {
+            print("success")
+            //success asset
+            trueBtn.isHidden = true
+            falseBtn.isHidden = true
+            failureLbl.isHidden = true
+            //failure asset
+            succesLbl.isHidden = false
+            nextBtn.isHidden = false
+            starView.isHidden = false
+        }else if alertType == .failure{
+            print("failure")
+            //failure asset
+            succesLbl.isHidden = true
+            nextBtn.isHidden = true
+            starView.isHidden = true
+            //success asset
+            trueBtn.isHidden = false
+            falseBtn.isHidden = false
+            failureLbl.isHidden = false
+        }else{
+            print("error")
+        }
+        UIApplication.shared.keyWindow?.addSubview(parentView)
+    }
+
+}
