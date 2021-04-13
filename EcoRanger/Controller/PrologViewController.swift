51cbee9e3bed9c152b9e51524e7f421dfc7e2261
EcoRanger <adiputrachristian17@gmail.com>
2021-04-13 20:42:28 +0700

    - rapiin data

--- EcoRanger/Controller/PrologViewController.swift
+++ EcoRanger/Controller/PrologViewController.swift
@@ -12,12 +12,20 @@ class PrologViewController: UIViewController {
     var chapterModel: Thumbnail?
     var ButtonPressSFX = AVAudioPlayer()
     var SFXAllowStatus = true
-    
+    @IBOutlet weak var thmbImage: UIView!
+    @IBOutlet weak var thumbnailImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         labelChapter.text = chapterModel?.chapter
+        thumbnailImage.image = UIImage(named: chapterModel!.imgthumbnail)
+        //thumbnailImage.layer.cornerRadius = 20.0
+        thumbnailImage.applyshadowWithCorner(containerView: thmbImage, cornerRadious: 10.0, shadowOpacity: 1)
+//        thumbnailImage.layer.shadowColor = UIColor.black.cgColor
+//        thumbnailImage.layer.shadowOpacity = 1
+//        thumbnailImage.layer.shadowOffset = .zero
+//        thumbnailImage.layer.shadowRadius = 20
+        
     }
-    
     
     
     @IBOutlet weak var labelChapter: UILabel!
@@ -58,11 +66,6 @@ class PrologViewController: UIViewController {
             })
         }
     }
-//    
-//    private func setupView() {
-//        guard let dataModel = chapterModel else { return }
-//        print(dataModel.chapter)
-//    }
     
     
     
